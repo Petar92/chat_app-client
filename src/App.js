3aba4b3dcf61d45f5af435a0084f3edc3f5aba90
repic <repic92@gmail.com>
2020-05-18 22:11:29 +0200

    first commit, basic client-side functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SockJsClient from 'react-stomp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+      <div>
+          <h1>test</h1>
+		  <SockJsClient url='http://localhost:8080/ws/'
+    topics={['/topic/user']}
+    onConnect={() => {
+        console.log("connected");
+    }}
+    onDisconnect={() => {
+        console.log("Disconnected");
+    }}
+    onMessage={(msg) => {
+        console.log(msg);
+    }}
+    //ref={(client) => {
+      //  this.clientRef = client
+//}}
+/>	  
+      </div>
+  )}
 export default App;
